7534d623dec3da3b1d1c10d6d632c1f41b31b8c6
Hitendra Nishar <hnishar@amazon.com>
2021-10-19 11:14:55 -0400

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,20 @@
+import { Construct } from 'constructs';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+
+export class CdkWorkshopStack extends Construct {
+  constructor(scope: cdk.App, id: string) {
+    super(scope, id);
+
+    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
+    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+
+    topic.addSubscription(new subs.SqsSubscription(queue));
+
+  }
+}
